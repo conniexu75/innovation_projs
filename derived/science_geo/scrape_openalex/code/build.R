42e1fe783ca13b2debc1b22473336195c3f62695
conniexu75 <conniexu75@gmail.com>
2023-07-26 14:52:31 -0400

    update R code

--- derived/science_geo/scrape_openalex/code/build.R
+++ derived/science_geo/scrape_openalex/code/build.R
@@ -16,51 +16,76 @@ pmid_file <- read_dta('../output/list_of_pmids_newfund_jrnls.dta')
 nr <- nrow(pmid_file)
 split_pmid <- split(pmid_file, rep(1:ceiling(nr/5000), each = 5000, length.out=nr))
 num_file <- length(split_pmid)
-for (j in 53:55) {
+for (j in 1:num_file) {
     ## pull open alex data from pmids
     works_from_pmids <- oa_fetch(
       entity = "works",
       ids.pmid  = split_pmid[[j]] %>%  mutate(pmid = as.character(pmid)) %>% pull(pmid),
-      verbose = TRUE
+      verbose = TRUE,
+      output = "list"
     )
-    ## get open alex id and pmid xwalk
-    pmid_list = works_from_pmids %>% select(ids)
-    id_xwalk <- lapply(1:nrow(pmid_list) , function(i) {
-        alex_id <- subset(pmid_list[["ids"]][[i]], name == "openalex")[2] %>% data.frame()
-        pmid <- subset(pmid_list[["ids"]][[i]], name == "pmid")[2] %>% data.frame()
-        cbind(alex_id, pmid)
-    }) %>% bind_rows() %>%
-    rename("id" = "value...1", 
-            "pmid" = "value...2") %>%
-            mutate(id = str_replace(id, "https://openalex.org/",""),
-                    pmid = str_replace(pmid, "https://pubmed.ncbi.nlm.nih.gov/","")) %>% as.data.frame()
-    write.dta(id_xwalk, paste0("../output/openalex_pmid_xwalk",as.character(j),".dta"))
-
     ## pull open_alex author ids associated with each author for each paper
-    all_authors = works_from_pmids %>% select(id, author) 
-    au_ids<- lapply(1:nrow(all_authors), function(i) {
-        if (is.na(all_authors[["author"]][[i]]) == FALSE) {
-            print(i)
-            ids <- replicate(n=length(all_authors[["author"]][[i]][["au_id"]]), all_authors[["id"]][[i]]) %>% data.frame
-            which_athr <- ave(1:length(all_authors[["author"]][[i]][["au_id"]]), ids, FUN = seq_along)
-            authors <- all_authors[["author"]][[i]][["au_id"]] %>% data.frame()
-            name <- all_authors[["author"]][[i]][["au_display_name"]] %>% data.frame()
-            inst_name <- all_authors[["author"]][[i]][["institution_display_name"]] %>% data.frame()
-            inst_id <- all_authors[["author"]][[i]][["institution_id"]] %>% data.frame()
-            which_athr <- ave(1:length(all_authors[["author"]][[i]][["au_id"]]), ids, FUN = seq_along)
-            cbind(ids, authors, name, inst_name, inst_id, which_athr)
-              }}) %>% 
-                bind_rows() %>%
-                rename("id" = "....1", 
-                "author_id" = "....2", 
-                "author_name" = "....3",
-                "inst_name" = "....4", 
-                "inst_id" = "....5") %>%
-                mutate(id = str_replace(id, "https://openalex.org/",""),
-                        author_id= str_replace(author_id, "https://openalex.org/", ""),
-                        inst_id = str_replace(inst_id, "https://openalex.org/","")) %>% as.data.frame()
-        write.dta(au_ids, paste0("../output/openalex_authors_xwalk", as.character(j), ".dta"))
+    N_articles <- length(works_from_pmids)
+    au_ids <- lapply(1:N_articles, function(i) {
+      ids <- replicate(n=length(works_from_pmids[[i]][["authorships"]]), works_from_pmids[[i]][["id"]]) %>% data.frame
+      pmid <- replicate(n=length(works_from_pmids[[i]][["authorships"]]), works_from_pmids[[i]][["ids"]][["pmid"]]) %>% data.frame
+      which_athr <- ave(1:length(works_from_pmids[[i]][["authorships"]]), ids, FUN = seq_along) %>% data.frame
+      N_athrs <- length(works_from_pmids[[i]][["authorships"]])
+      num_affls <- list()
+      athr_id <- list()
+      athr_name <- list()
+      num_affls <-  lapply(1:N_athrs, function(j) {
+        append(num_affls, length(works_from_pmids[[i]][["authorships"]][[j]][["institutions"]]))
+      })  %>% data.frame %>% t()
+      athr_id <-  lapply(1:N_athrs, function(j) {
+        append(athr_id, works_from_pmids[[i]][["authorships"]][[j]][["author"]][["id"]])
+      })  %>% data.frame %>% t()
+      athr_name <-  lapply(1:N_athrs, function(j) {
+        append(athr_name, works_from_pmids[[i]][["authorships"]][[j]][["author"]][["display_name"]])
+      })  %>% data.frame %>% t()
+      cbind(ids, pmid, which_athr, athr_id, athr_name, num_affls)
+    }) %>% bind_rows()
+    colnames(au_ids) <- c("id","pmid","which_athr","athr_id", "athr_name", "num_affls")
+    au_ids <- au_ids %>% uncount(num_affls)
+    inst <- list()
+    inst_id <- list()
+    for(i in 1:N_articles) {
+      N_athrs <- length(works_from_pmids[[i]][["authorships"]])
+      for(j in 1:N_athrs) {
+        for(k in 1:length(works_from_pmids[[i]][["authorships"]][[j]][["institutions"]])) {
+          inst<-append(inst, works_from_pmids[[i]][["authorships"]][[j]][["institutions"]][[k]][["display_name"]])
+          inst_id<-append(inst_id, works_from_pmids[[i]][["authorships"]][[j]][["institutions"]][[k]][["id"]])
+        }
+      }
+    }
+    
+    inst <- inst %>% data.frame %>% t()
+    inst_id <- inst_id %>% data.frame %>% t()
+    affl_list <- cbind(au_ids,inst, inst_id) %>% 
+      group_by(id, which_athr) %>% 
+      mutate(which_affl = 1:n(),
+             id = str_replace(id, "https://openalex.org/",""),
+             pmid = str_replace(pmid, "https://pubmed.ncbi.nlm.nih.gov/", ""),
+             athr_id= str_replace(athr_id, "https://openalex.org/", ""),
+             inst_id = str_replace(inst_id, "https://openalex.org/",""))
+    write.dta(affl_list, paste0("../output/openalex_authors", as.character(j), ".dta"))
+    ## get mesh terms
+    mesh_terms <- lapply(1:N_articles, function(i) {
+      ids <- replicate(n=length(works_from_pmids[[i]][["mesh"]]), works_from_pmids[[i]][["id"]]) %>% data.frame
+      which_mesh <- ave(1:length(works_from_pmids[[i]][["mesh"]]), ids, FUN = seq_along) %>% data.frame
+      N_mesh <- length(works_from_pmids[[i]][["mesh"]])
+      terms <- list()
+      major_topic <- list()
+      terms <-  lapply(1:N_mesh, function(j) {
+        append(terms, works_from_pmids[[i]][["mesh"]][[j]][["descriptor_name"]])
+      }) %>% data.frame %>% t()
+      major_topic <-  lapply(1:N_mesh, function(j) {
+        append(major_topic, works_from_pmids[[i]][["mesh"]][[j]][["is_major_topic"]])
+      })  %>% data.frame %>% t()
+      cbind(ids, which_mesh, mesh_terms, major_topic)
+    }) %>% bind_rows()
+    colnames(mesh_terms) <- c("id","which_mesh","term", "is_major_topic")
+    mesh_terms <- mesh_terms %>% mutate(id = str_replace(id, "https://openalex.org/",""))
+    write.dta(mesh_terms, paste0("../output/mesh_terms", as.character(j), ".dta"))
 }
-
-unique_authors <- unique(au_ids$authors) 
 
